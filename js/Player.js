fa717d93e05b6163d9374f7e9d8a0e79b16ad870
aanchal-31 <73495899+aanchal-31@users.noreply.github.com>
2021-05-20 15:00:19 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,95 @@
+class Player{
+    constructor()
+    {
+
+    }
+
+getCount()
+{
+    var playerCountRef=database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+        playerCount=data.val();
+    })
+}
+
+updateCount(count){
+    database.ref('/').update({
+        playerCount:count
+    })
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
